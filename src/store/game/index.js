fc67c15cace12fc78e0a9de0f30e2fcb28218ed3
unknown <patryk.maternicki@gmail.com>
2021-02-10 21:07:32 +0100

    Updates

--- src/store/game/index.js
+++
@@ -1,4 +0,0 @@
-import gameReducers from './reducers';
-export {default as GameTypes } from './types';
-export {default as GameActions } from './actions';
-export default gameReducers;
--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import GameReducer from './game';
-
-const createReducer = () => combineReducers({
-  game: GameReducer
-})
-
-export default createReducer;
